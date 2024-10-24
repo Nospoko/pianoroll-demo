1c832d988896ffcf205afbc5faa625e86aa603b4
Tomek Roszczynialski <roszcz@users.noreply.huggingface.co>
2024-10-24 20:58:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ from fortepyan import MidiPiece
 dataset = load_dataset("epr-labs/maestro-sustain-v2", split="train")
 piece = MidiPiece.from_huggingface(dataset[312])
 
-streamlit_pianoroll.from_fortepyan(piece[:5000])
+streamlit_pianoroll.from_fortepyan(piece[:500])
