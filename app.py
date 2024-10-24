771b98d3764dff0059c7d66cdf754959683b6651
Tomek Roszczynialski <roszcz@users.noreply.huggingface.co>
2024-10-24 20:55:07 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit_pianoroll
+from datasets import load_dataset
+from fortepyan import MidiPiece
+
+dataset = load_dataset("epr-labs/maestro-sustain-v2", split="train")
+piece = MidiPiece.from_huggingface(dataset[312])
+
+streamlit_pianoroll.from_fortepyan(piece)
