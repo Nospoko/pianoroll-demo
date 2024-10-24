51f2390dcd493aaf5ba27c6813ab95a4d7e5be08
Tomek Roszczynialski <roszcz@users.noreply.huggingface.co>
2024-10-24 21:01:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from datasets import load_dataset
 from fortepyan import MidiPiece
 
 dataset = load_dataset("epr-labs/maestro-sustain-v2", split="train")
-piece = MidiPiece.from_huggingface(dataset[312])
+idx = st.number_input(
+    label="Piece IDX",
+    min_value=0,
+    max_value=len(dataset) - 1,
+    value=420,
+)
+piece = MidiPiece.from_huggingface(dataset[idx])
 
 streamlit_pianoroll.from_fortepyan(piece[:500])
